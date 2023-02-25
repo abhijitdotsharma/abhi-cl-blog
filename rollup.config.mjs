eadcc2b06203aae6355d87473b516324df272cde
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2023-02-25 19:14:09 +0530

    terser and peerDeps added

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,6 +6,9 @@ import packageJson from "./package.json" assert { type: "json" };
 
 import postcss from "rollup-plugin-postcss";
 
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
 export default [
     {
       input: "src/index.ts",
@@ -22,12 +25,14 @@ export default [
         },
       ],
       plugins: [
+        peerDepsExternal(),
         resolve(),
         commonjs(),
         typescript({ tsconfig: "./tsconfig.json" }),
         postcss({
           plugins: []
-        })
+        }),
+        terser(),
       ],
     },
     {
